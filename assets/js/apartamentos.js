a7d79b47e0e3fd6e7acb605b92481edf137a9bdf
RTeixe0 <renanteixeira121@outlook.com>
2025-06-15 16:11:43 -0300

    update das paginas

--- assets/js/apartamentos.js
+++ assets/js/apartamentos.js
@@ -1,6 +1,6 @@
 const apartamentos = [
-  { nome: "Reserva DNA", arquivo: "ap1.html" },
-  { nome: "Vista Piscina", arquivo: "ap2.html" },
-  { nome: "Vista Mata", arquivo: "ap3.html" },
+  { nome: "Reserva DNA - Frente ao Mar", arquivo: "ap1.html" },
+  { nome: "Grand Beach - Vista Mata", arquivo: "ap2.html" },
+  { nome: "Grand Beach - Vista Piscina", arquivo: "ap3.html" },
   // Adicione novos aqui
 ];
