8c0bb334167459495c429afde2613db9ef1859d7
RTeixe0 <renanteixeira121@outlook.com>
2025-06-17 18:35:55 -0300

    Comecando a implementar primeira pagina do riviera

--- assets/js/apartamentos.js
+++ assets/js/apartamentos.js
@@ -2,5 +2,6 @@ const apartamentos = [
   { nome: "Reserva DNA - Frente ao Mar", arquivo: "ap1.html" },
   { nome: "Grand Beach - Vista Mata", arquivo: "ap2.html" },
   { nome: "Grand Beach - Vista Piscina", arquivo: "ap3.html" },
+  { nome: "Riviera", arquivo: "ap4.html" },
   // Adicione novos aqui
 ];
