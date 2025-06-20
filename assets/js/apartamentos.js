9a43b3e713429480bd348300d4113f318ca41970
RTeixe0 <renanteixeira121@outlook.com>
2025-06-20 16:09:34 -0300

    css ajustado, imagens dos outros apartamentos incluida

--- assets/js/apartamentos.js
+++ assets/js/apartamentos.js
@@ -1,5 +1,5 @@
 const apartamentos = [
-  { nome: "Reserva DNA - Frente ao Mar", arquivo: "ap1.html" },
+  { nome: "Reserva DNA 13.1.4 - Frente ao Mar", arquivo: "ap1.html" },
   { nome: "Grand Beach - Vista Mata", arquivo: "ap2.html" },
   { nome: "Grand Beach - Vista Piscina", arquivo: "ap3.html" },
   { nome: "Riviera de Ubatuba", arquivo: "ap4.html" },
