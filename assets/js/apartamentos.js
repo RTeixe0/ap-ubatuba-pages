3488f825aae7bdd249d7164c64353616c381b09e
RTeixe0 <renanteixeira121@outlook.com>
2025-06-26 04:33:46 -0300

    upload

--- assets/js/apartamentos.js
+++ assets/js/apartamentos.js
@@ -2,10 +2,10 @@ const apartamentos = [
   { nome: "Reserva DNA 13.1.4 - Frente ao Mar", arquivo: "DNA_13.1.4.html" },
   { nome: "Grand Beach - Vista Mata", arquivo: "GBMata.html" },
   { nome: "Grand Beach - Vista Piscina", arquivo: "GBPiscina.html" },
-  { nome: "Riviera de Ubatuba", arquivo: "Riviera.html" },
   { nome: "Reserva DNA 3.0.1", arquivo: "DNA_3.0.1.html" },
   { nome: "Reserva DNA 9.2.3", arquivo: "DNA_9.2.3.html" },
   { nome: "Reserva DNA 10.4.2", arquivo: "DNA_10.4.2.html" },
+  { nome: "Riviera de Ubatuba", arquivo: "Riviera.html" },
   { nome: "Grand Beach - Cobertura", arquivo: "GBCobertura.html" },
   { nome: "Costa Atlântica - Cobertura", arquivo: "CACobertura.html" },
 ];
